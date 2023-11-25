c68a93e2e70b538fb877eac722079ca416f485f8
Mohaimen Al Rashid <kdmars0168@gmail.com>
2023-11-25 11:56:08 +0600

    DKeeper App v2

---
+++ src/dkeeper_frontend/src/components/App.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+import { dkeeper_backend } from "../../../declarations/dkeeper_backend";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      dkeeper_backend.createNote(newNote.title, newNote.content);
+      return [newNote, ...prevNotes];
+    });
+  }
+
+  useEffect(() => {
+    console.log("dkeeper_backend object:", dkeeper_backend); // Check the entire object
+    console.log("removeNote function:", dkeeper_backend.removeNote); // Check if removeNote is present
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const notesArray = await dkeeper_backend.readNotes();
+    setNotes(notesArray);
+  }
+
+  function deleteNote(id) {
+    dkeeper_backend.removeNote(id);
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
